a99400805bb9e62e2a8ee7ad10d77d3fc6a7088f
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2022-08-31 08:45:48 -0700

    fix(release): run npm plugin before git so new package.json version gets committed

--- release.config.js
+++ release.config.js
@@ -10,16 +10,16 @@ module.exports = {
             }
         ],
         [
+            '@semantic-release/npm',
+            {
+                tarballDir: 'pack'
+            }
+        ],
+        [
             '@semantic-release/git',
             {
                 // eslint-disable-next-line no-template-curly-in-string
                 message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-            }
-        ],
-        [
-            '@semantic-release/npm',
-            {
-                tarballDir: 'pack'
             }
         ],
         [
