2990abafd1f8cb5eaa01ed5854f47de90500f4c6
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2022-10-03 11:33:40 -0700

    chore(lint): break up long config line to make lint happy

--- release.config.js
+++ release.config.js
@@ -19,7 +19,9 @@ module.exports = {
             '@semantic-release/git',
             {
                 // eslint-disable-next-line no-template-curly-in-string
-                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${(nextRelease.notes.length < 32000) ? nextRelease.notes : (nextRelease.notes.slice(0,32000) + "...\\n\\n(Notes too long. Truncated.)")}'
+                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n' +
+                    '${(nextRelease.notes.length < 32000) ? ' +
+                    'nextRelease.notes : (nextRelease.notes.slice(0,32000) + "...\\n\\n(Notes too long. Truncated.)")}'
             }
         ],
         [
