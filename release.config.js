7d4035411704dc7d7b1f239584869c886c003079
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2022-10-03 08:57:46 -0700

    fix(release): limit release git commit message to 32k
    This fixes "spawn E2BIG" errors with VERY long commit messages, which
    can happen with the first release after enabling semantic-release.
    
    We should consider whether it's possible to merge multiple dependency
    updates into one line, since in our situation that's the source of many
    redundant lines in the changelog.

--- release.config.js
+++ release.config.js
@@ -19,7 +19,7 @@ module.exports = {
             '@semantic-release/git',
             {
                 // eslint-disable-next-line no-template-curly-in-string
-                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${(nextRelease.notes.length < 32000) ? nextRelease.notes : (nextRelease.notes.slice(0,32000) + "...\\n\\n(Notes too long. Truncated.)")}'
             }
         ],
         [
