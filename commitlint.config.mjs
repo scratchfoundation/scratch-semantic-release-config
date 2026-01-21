d6a50203342753e9578c5d2eb3765662180e8e4b
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2026-01-21 14:13:13 -0800

    chore: convert commitlint config to ESM

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,4 +1,7 @@
-module.exports = {
+/**
+ * @type {import('@commitlint/types').UserConfig}
+ */
+export default {
     extends: ['@commitlint/config-conventional'],
     ignores: [message => message.startsWith('chore(release):')]
 };
